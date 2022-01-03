9009763f7070a07439db382e39a03ca1231fae9b
Jim <jimiscoming723@gmail.com>
2022-01-03 19:16:35 +0800

    xxx

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <!-- <v-img :src="logo" class="my-3" contain height="200" /> -->
   <div class="text-center">
+      {{msg}}
     <v-pagination v-model="page" :length="4" rounded="circle"></v-pagination>
   </div>
 </template>
@@ -10,7 +11,8 @@ import logo from "../assets/logo.svg";
 export default {
   name: "Home",
   data: () => ({
-    //
+    msg: "hello world",
+    
   }),
 };
 </script>
