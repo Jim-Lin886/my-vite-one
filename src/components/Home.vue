c61934cf1fa04f6598871029128db8a73f911038
Jim <jimiscoming723@gmail.com>
2022-01-03 18:58:17 +0800

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- <v-img :src="logo" class="my-3" contain height="200" /> -->
+  <div class="text-center">
+    <v-pagination v-model="page" :length="4" rounded="circle"></v-pagination>
+  </div>
+</template>
+
+<script>
+import logo from "../assets/logo.svg";
+export default {
+  name: "Home",
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style scoped></style>
